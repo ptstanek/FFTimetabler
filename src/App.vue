07172a4a00edb348851a007ed01ac038d042b722
Patrick Stanek <patrickstanek96@hotmail.com>
2026-08-23 12:11:34 +0100

    adding to VCS

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const counter = ref(0);
+const clickCallback = (): void => {
+  counter.value++;
+};
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">{{counter}}</h1>
+    <button @click="clickCallback">button</button>
+  </div>
+</template>
+
+<style scoped></style>
